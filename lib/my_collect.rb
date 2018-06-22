c556135df509c04b1b1e7bcb166705146a238b7b
William Chabla <w_chabla@hotmail.com>
2018-06-22 02:08:55 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
-
+def my_collect(array)
+  if block_given?
+    i = 0
+    new_array = []
+    
+    while i < array.length do
+      new_array << yield(array[i])
+      i += 1
+    end
+    new_array
+  else
+    array
+  end
+end
 
